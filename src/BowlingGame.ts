8509783cdd4863bcea949f22b2d2d4820cb7ebc3
Teagan Durtschi <hugsar@gmail.com>
2018-11-17 11:29:17 -0500

    tcr

--- src/BowlingGame.ts
+++ src/BowlingGame.ts
@@ -6,31 +6,38 @@ export type Game = Frame[];
 function BowlingGame(game: Game) {
 	let total = 0;
 	game.forEach((frame, index) => {
-		if(regularFrame(frame)){
-			// Regular frame
-			total += ((frame[0] as number) + (frame[1] as number));
-		} else if (spare(frame)) {
-			// Spare
+		total += getScoreForFrame(game, index);
+	});
+	return total;
+}
+
+const getScoreForFrame = (game: Game, index: number) => {
+	const frame: Frame = game[index];
+	let total: number = 0;
+	if(regularFrame(frame)){
+		// Regular frame
+		total += ((frame[0] as number) + (frame[1] as number));
+	} else if (spare(frame)) {
+		// Spare
+		total += 10;
+		total += (game[index+1][0] as number);
+	} else {
+		// Strike
+		total += 10;
+		if(strike(game[index+1])){
 			total += 10;
-			total += (game[index+1][0] as number);
-		} else {
-			// Strike
-			total += 10;
-			if(strike(game[index+1])){
-				total += 10;
-				if(strike(game[index+2]))
-				{
-					total += 10;
-				} else {
-					total += (game[index+2][0] as number);
-				}
-			} else if (spare(game[index+1])){
+			if(strike(game[index+2]))
+			{
 				total += 10;
 			} else {
-				total += (game[index+1][0] as number) + (game[index+1][1] as number);
+				total += (game[index+2][0] as number);
 			}
+		} else if (spare(game[index+1])){
+			total += 10;
+		} else {
+			total += (game[index+1][0] as number) + (game[index+1][1] as number);
 		}
-	});
+	}
 	return total;
 }
 
