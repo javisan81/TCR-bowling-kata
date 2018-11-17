225059ffd67e403978b81fce9b17fe853c3b4edf
Teagan Durtschi <hugsar@gmail.com>
2018-11-17 10:07:41 -0500

    tcr

--- spec/BowlingGame.spec.ts
+++ spec/BowlingGame.spec.ts
@@ -15,5 +15,21 @@ describe("BowlingGame calculates the correct score", () => {
 			[0,0],
 		]
         expect(BowlingGame(score)).toBe(0);
-    });
+	});
+	
+	it("All rolls are 9", () => {
+		var score: Game = [
+			[9,0],
+			[1,8],
+			[2,7],
+			[3,6],
+			[4,5],
+			[5,4],
+			[6,3],
+			[7,2],
+			[8,1],
+			[0,9],
+		]
+        //expect(BowlingGame(score)).toBe(0);
+	})
 });
