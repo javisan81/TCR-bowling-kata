d560e35e016d84bbe6f55ff1d772e023ac17f345
Teagan Durtschi <hugsar@gmail.com>
2018-11-17 09:50:01 -0500

    initial commit

---
+++ spec/setup.spec.ts
@@ -0,0 +1,5 @@
+describe("jasmine", () => {
+    it("runs tests", () => {
+        expect(fail).toBe(true);
+    });
+});
---
+++ tcr.ts
@@ -0,0 +1,66 @@
+var spawn = require('cross-spawn');
+
+new Promise(function (resolve, reject) {
+	var child = spawn(`yarn test`, []);
+
+	child.stdout.on('data', function (data: any) {
+		process.stdout.write(data);
+	});
+
+	child.stderr.on('data', function (data: any) {
+		process.stderr.write(data);
+	});
+
+	child.on('error', function (data: any) {
+		reject(`git error! ${data}`);
+	});
+
+	child.on('exit', function () {
+		resolve('success!');
+	});
+})
+.then((msg) => console.log(msg))
+.then(() => {
+	return new Promise(function (resolve, reject) {
+		var child = spawn(`git add * && git commit -m "tcr"`, []);
+	
+		child.stdout.on('data', function (data: any) {
+			process.stdout.write(data);
+		});
+	
+		child.stderr.on('data', function (data: any) {
+			process.stderr.write(data);
+		});
+	
+		child.on('error', function (data: any) {
+			reject(`git error! ${data}`);
+		});
+	
+		child.on('exit', function () {
+			resolve('success!');
+		});
+	})
+})
+.catch((msg) => {
+	console.log(msg)
+
+	return new Promise(function (resolve, reject) {
+		var child = spawn(`git reset --hard`, []);
+	
+		child.stdout.on('data', function (data: any) {
+			process.stdout.write(data);
+		});
+	
+		child.stderr.on('data', function (data: any) {
+			process.stderr.write(data);
+		});
+	
+		child.on('error', function (data: any) {
+			reject(`git error! ${data}`);
+		});
+	
+		child.on('exit', function () {
+			resolve('success!');
+		});
+	})
+});
