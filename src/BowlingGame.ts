3183a429d1c6a42792d0dbe4c120f3ffc219b7a0
Teagan Durtschi <hugsar@gmail.com>
2018-11-18 09:51:24 -0500

    tcr

--- src/BowlingGame.ts
+++ src/BowlingGame.ts
@@ -15,40 +15,38 @@ function BowlingGame(game: Game) {
 const getScoreForFrame = (frame: Frame | EndFrame, nextFrame: Frame | EndFrame, nextnextFrame: Frame | EndFrame) => {
 	if(lastFrame(frame)){
 		return scoreLastFrame(frame as EndFrame);
-	}
-
-	if(regularFrame(frame)){
-		// Regular frame
+	} else if(regularFrame(frame)){
+		// Regular frame (no strikes or spares)
 		return ((frame[0] as number) + (frame[1] as number));
 	} else if (spare(frame)) {
 		// Spare
 		return 10 + (nextFrame[0] as number);
 	} else {
 		// Strike
-		let total = 0;
-		total += 10;
-		if(strike(nextFrame)){
-			if(lastFrame(nextFrame)){
-				return 20 + (nextFrame[1] as number);
-			}
-			total += 10;
-			if(strike(nextnextFrame))
-			{
-				total += 10;
-			} else {
-				total += (nextnextFrame[0] as number);
-			}
-		} else if (spare(nextFrame)){
-			total += 10;
-		} else {
-			total += (nextFrame[0] as number) + (nextFrame[1] as number);
-		}
-		return total;
+		return scoreStrike(frame, nextFrame, nextnextFrame);
 	}
 }
 
-const strikeOnLastFrameScore = (frame : EndFrame) => {
-	if(frame[0] === "x") return 10 + (frame[1] as number);
+const scoreStrike = (frame: Frame | EndFrame, nextFrame: Frame | EndFrame, nextnextFrame: Frame | EndFrame) => {
+	let total = 0;
+	total += 10;
+	if(strike(nextFrame)){
+		if(lastFrame(nextFrame)){
+			return 20 + (nextFrame[1] as number);
+		}
+		total += 10;
+		if(strike(nextnextFrame))
+		{
+			total += 10;
+		} else {
+			total += (nextnextFrame[0] as number);
+		}
+	} else if (spare(nextFrame)){
+		total += 10;
+	} else {
+		total += (nextFrame[0] as number) + (nextFrame[1] as number);
+	}
+	return total;
 }
 
 const scoreLastFrame = (frame : EndFrame) => {
